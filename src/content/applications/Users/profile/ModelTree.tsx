7556cfeb48031c9b125ac70d30932c2c4e9572d5
luanhv <luanhv@vng.com.vn>
2023-03-27 09:42:10 +0700

    gf

--- src/content/applications/Users/profile/ModelTree.tsx
+++ src/content/applications/Users/profile/ModelTree.tsx
@@ -1,36 +1,100 @@
 
-import TreeItem from '@mui/lab/TreeItem';
-import { Props } from 'react-apexcharts';
+import {
+    Box,
+    Typography,
+    Card,
+    CardHeader,
+    Divider,
+    Avatar,
+    useTheme,
+    styled,
+    TextField,
+    Button
+} from '@mui/material';
+
 import { FC } from 'react';
-
-
+import { useState } from 'react';
+import ShoppingBagTwoToneIcon from '@mui/icons-material/ShoppingBagTwoTone';
+import FavoriteTwoToneIcon from '@mui/icons-material/FavoriteTwoTone';
+import StarTwoToneIcon from '@mui/icons-material/StarTwoTone';
+import TreeView from '@mui/lab/TreeView';
+import TreeItem from '@mui/lab/TreeItem';
+import ExpandMoreIcon from '@mui/icons-material/ExpandMore';
+import ChevronRightIcon from '@mui/icons-material/ChevronRight';
 interface ModelTreeProps {
     model?: Object;
     modelName: string
 }
 const ModelTree: FC<ModelTreeProps> = ({ model, modelName }) => {
+    const [expanded, setExpanded] = useState<string[]>([]);
+    const [arrExpend, setArrExpend] = useState<string[]>([]);
+    const [id, setId] = useState<number>(1);
+    const handleToggle = (event: React.SyntheticEvent, nodeIds: string[]) => {
+        setExpanded(nodeIds);
+    };
 
 
+
+    const handleExpandClick = () => {
+        setExpanded((oldExpanded) => {
+            if (oldExpanded.length === 0) return arrExpend
+            setArrExpend([])
+            return []
+        }
+        );
+
+    };
+
+    const getNewId = () => {
+        let oldId = id;
+        setId(oldId + 1);
+        return oldId;
+    }
+
+    const addToExpand = (id) => { }   // setArrExpend(oldExpanded => [...oldExpanded, id])}
+    console.log("reload")
     const createTreeArray = (key: string, arr: Array<any>) => {
-        return {}
+        // let idExpend = getNewId();
+        // addToExpand(idExpend);
+        return (
+            <TreeItem nodeId={key + ""} key={key + ""} label={key.toString() + ":[]"} >
+                {
+                    arr.map((data, idx) => {
+                        if (Array.isArray(data)) {
+                            return createTreeArray(idx + "", data)
+                        }
+                        if (typeof data === "object") {
+                            return createTreeObject(idx + "", data)
+                        }
+                        // let idExpend = getNewId();
+                        return (<TreeItem nodeId={idx + ""} key={idx + ""} label={idx + ":" + data} />)
+                    })
+                }
+            </TreeItem>
+
+        )
     }
 
 
 
 
     const createTreeObject = (key: string, obj: Object) => {
-        console.log("obj: ", key, JSON.stringify(obj))
+        // let idExpend = getNewId();
+        // addToExpand(idExpend);
         return (
-            <TreeItem nodeId="-1" key={key} label={key.toString()} >
+            <TreeItem nodeId={key + ""} key={key + ""} label={key.toString() + ":"} >
+
                 {
                     Object.keys(obj).map((k, v) => {
-                        // if (Array.isArray(v)) {
-                        //     return createTreeArray(k, obj[k])
-                        // }
-                        // if (typeof v === "object") {
-                        //     return createTreeObject(k, obj[k])
-                        // }
-                        <TreeItem nodeId="-1" key={k} label={k + ":" + v.toString()} />
+                        let data = obj[k];
+                        if (Array.isArray(data)) {
+                            return createTreeArray(k, data)
+                        }
+                        if (typeof data === "object") {
+                            return createTreeObject(k, data)
+                        }
+                        // let idExpend = getNewId();
+                        return (<TreeItem nodeId={k + ""} key={k + ""} label={k + ":" + data} />)
                     })
                 }
             </TreeItem>
@@ -41,7 +105,22 @@ const ModelTree: FC<ModelTreeProps> = ({ model, modelName }) => {
 
     return (
         <>
-            {createTreeObject(modelName, model)}</>
+            <Box sx={{ mb: 1 }}>
+                <Button onClick={handleExpandClick}>
+                    {'Collapse all'}
+                </Button>
+            </Box>
+            <TreeView
+                aria-label="controlled"
+                defaultCollapseIcon={<ExpandMoreIcon />}
+                defaultExpandIcon={<ChevronRightIcon />}
+                expanded={expanded}
+                onNodeToggle={handleToggle}
+                multiSelect
+            >
+                {createTreeObject(modelName, model)}
+            </TreeView></>
+
     );
 };
 
--- src/content/applications/Users/profile/RecentActivity.tsx
+++ src/content/applications/Users/profile/RecentActivity.tsx
@@ -11,14 +11,8 @@ import {
   Button
 } from '@mui/material';
 
-import ShoppingBagTwoToneIcon from '@mui/icons-material/ShoppingBagTwoTone';
-import FavoriteTwoToneIcon from '@mui/icons-material/FavoriteTwoTone';
-import StarTwoToneIcon from '@mui/icons-material/StarTwoTone';
-import TreeView from '@mui/lab/TreeView';
-import TreeItem from '@mui/lab/TreeItem';
-import ExpandMoreIcon from '@mui/icons-material/ExpandMore';
-import ChevronRightIcon from '@mui/icons-material/ChevronRight';
-import { useState } from 'react';
+
+
 import ModelTree from './ModelTree';
 const AvatarPrimary = styled(Avatar)(
   ({ theme }) => `
@@ -30,27 +24,20 @@ const AvatarPrimary = styled(Avatar)(
 );
 let userModel = {
   userId: 111,
-  gold: 3434,
+  name: "luanhuynh",
+  listGold: [1000, 2000, 300],
+  listModel: {
+    model: ["model1", "mode2", {
+      "name": "mode3",
+      "value": 4
+    }]
+  }
 
 }
 function RecentActivity() {
   const theme = useTheme();
-  const [expanded, setExpanded] = useState<string[]>([]);
-  const [selected, setSelected] = useState<string[]>([]);
-  const handleToggle = (event: React.SyntheticEvent, nodeIds: string[]) => {
-    setExpanded(nodeIds);
-  };
 
-  const handleSelect = (event: React.SyntheticEvent, nodeIds: string[]) => {
-    setSelected(nodeIds);
-  };
 
-  const handleExpandClick = () => {
-    setExpanded((oldExpanded) =>
-      oldExpanded.length === 0 ? ['1', '5', '6', '7'] : [],
-    );
-
-  };
 
   return (
     <Card>
@@ -62,31 +49,8 @@ function RecentActivity() {
       />
       <Divider />
       <Box sx={{ height: 600, width: 1, overflowY: 'auto' }}>
-        <Box sx={{ mb: 1 }}>
-          <Button onClick={handleExpandClick}>
-            {expanded.length === 0 ? 'Expand all' : 'Collapse all'}
-          </Button>
-        </Box>
-        <TreeView
-          aria-label="controlled"
-          defaultCollapseIcon={<ExpandMoreIcon />}
-          defaultExpandIcon={<ChevronRightIcon />}
-          expanded={expanded}
-          selected={selected}
-          onNodeToggle={handleToggle}
-          onNodeSelect={handleSelect}
-          multiSelect
-        >
-          <ModelTree model={userModel} modelName={"userModel"} />
-          <TreeItem nodeId="5" label="Documents">
-            <TreeItem nodeId="6" label="MUI">
-              <TreeItem nodeId="7" label="src">
-                <TreeItem nodeId="8" label="index.js" />
-                <TreeItem nodeId="9" label="tree-view.js" />
-              </TreeItem>
-            </TreeItem>
-          </TreeItem>
-        </TreeView>
+
+        <ModelTree model={userModel} modelName={"userModel"} />
       </Box>
     </Card>
   );
